5c53b6dd41b782318b16c62cc9b0feb42b7b3f59
webuttam <32165576+webuttam@users.noreply.github.com>
2024-08-24 15:13:23 +0530

    Update App.jsx
    App.jsx file update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Header from './components/main/Header'
 import Home from './components/home/Home'
 import Contact from './components/contact/Contact'
 import Footer from './components/main/Footer'
-import Contact from './components/contact/Contact'
 
 function App() {
 
