39eadfab877fa9e006cf9020141a3f48a4b63cd3
webuttam <32165576+webuttam@users.noreply.github.com>
2024-08-24 16:38:23 +0530

    Update Header.jsx
    Header edit

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <><Header /><Home /></>,
+      element: <><Home /></>,
     },
     {
       path: "/",
